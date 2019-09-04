d14efcad870da88e40852f452c1aa436e4ecfaa8
VincentXII <vincentwl1@hotmail.com>
2019-09-04 13:47:30 +0000

    Create Car.java and its no args constructor

--- Epstein.java
+++
@@ -1,7 +0,0 @@
-public class Epstein
-{
-    public static void main(String[] args)
-    {
-        System.out.println("Antarctica");
-    }
-}
--- HelloName.java
+++
@@ -1,8 +0,0 @@
-public class HelloName
-{
-    public static void main(String[] args)
-    {
-        String name = "Vincent";
-        System.out.println("Hello, " + name);
-    }
-}
