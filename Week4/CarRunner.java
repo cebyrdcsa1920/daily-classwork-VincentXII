2ed36adcaedb5ed973042fd80740672e00247805
VincentXII <vincentwl1@hotmail.com>
2019-09-11 14:11:14 +0000

    Add Tesla

--- Week4/CarRunner.java
+++ Week4/CarRunner.java
@@ -6,5 +6,13 @@ public class CarRunner
         //System.out.println(myMustang);
         myMustang.drive(5.5);
         System.out.println(myMustang.getAmountOfGas());
+        myMustang.drive(100.5);
+        System.out.println(myMustang.getAmountOfGas());
+        myMustang.fillUpGasTank(9);
+        System.out.println(myMustang.getAmountOfGas());
+
+        Car Tesla = new Car();
+        Tesla.drive(3.4);
+        System.out.println(Tesla.getAmountOfGas());
     }
 }
