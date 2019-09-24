58d63aeadda78307b7ecf9d5549881d637a05243
Vincent <vincentwl1@hotmail.com>
2019-09-24 14:13:00 +0000

    Add Convert Methods

--- Week7/TempConvert.java
+++ Week7/TempConvert.java
@@ -9,21 +9,45 @@ public class TempConvert
         isCelsius = true;
     }
 
-    public void convertToF
+    public void convertToF()
     {
         if(isCelsius)
         {
             temperature = temperature * 9 / 5 + 32;
+            isCelsius = false;
         }
     }
 
     public String toString()
     {
-        String result = "The current temperature is" + temperature + "degrees";
+        String result = "The current temperature is " + temperature + " degrees ";
         if (isCelsius)
+        {
+            result += "Celsius";
+        }
+        else
         {
             result += "Fahrenheit";
         }
         return result;
+    }
+
+    public static void main(String[] args)
+    {
+        TempConvert myTemp = new TempConvert(25.6);
+        System.out.println(myTemp);
+        myTemp.convertToF();
+        System.out.println(myTemp);
+        myTemp.convertToC();
+        System.out.println(myTemp);
+    }
+
+    public void convertToC()
+    {
+        if (!isCelsius)
+        {
+            temperature = (temperature - 32) * 5 / 9;
+            isCelsius = true;
+        }
     }
 }
